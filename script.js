b6f40ba337e53c6b4d04c6655c32ecf32ab5c73b
Marco Ferreira <marcoasf.com@gmail.com>
2022-09-13 11:57:43 -0500

    adding api edamam

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
-let example1 = 22
-let example2 = 10
-if(example1>example2)
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '5ef0a389f2msh11866c287896cb0p101bc7jsn1c10fa4b94f1',
+		'X-RapidAPI-Host': 'edamam-recipe-search.p.rapidapi.com'
+	}
+};
 
-let example3 = 1
-console.log("we can do it")
+fetch('https://edamam-recipe-search.p.rapidapi.com/search?q=chicken', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err));
