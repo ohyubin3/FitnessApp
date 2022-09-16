20b53ab510c3eb580fbdc7fbcfc61f74e2f6480e
Marco Ferreira <marcoasf.com@gmail.com>
2022-09-16 10:14:01 -0500

    cleaning script

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var recipeList = document.querySelector('ul');
 let proteinContainer = document.querySelector(".dropdown-menu")
 let recipeDetail = document.querySelector("li")
 let infoCard = document.querySelector("#infoCard")
-
+let calorieCard = document.querySelector("#calorieCard")
 console.log(recipeList)
 
 const nutritionAPI = {
@@ -14,7 +14,7 @@ const nutritionAPI = {
   },
 };
 
-
+// --------------Protein options and selections and calories below-------------
 let protein;
 function getApi(event) {
   if (event.target.matches("a")) {
@@ -24,66 +24,33 @@ function getApi(event) {
       .then(function (response) {
         return response.json();
       })
+
       .then(function (data) {
-        // console.log(data)
-        // console.log("hello")
-        // console.log(typeof data)
         let recipes = data.hits
-
         for (var i = 0; i < recipes.length; i++) {
-          // console.log("hello", i)
           var listItem = document.createElement('li');
-          // console.log(listItem)
-
-          // path for looping over lables
-          // console.log(recipes[i].recipe.label)
-  // consider doing innerhtml if you want links 
           listItem.dataset.cal = Math.floor(parseInt(recipes[i].recipe.calories))
-
           listItem.innerHTML = `${recipes[i].recipe.label}`
-          // console.log(listItem)
-
           recipeList.appendChild(listItem);
-          listItem.dataset.url = recipes[i].recipe.url
-          
+          listItem.dataset.url = recipes[i].recipe.url}});}}
+//----------------------------------- Protien optoins above--------------
 
-          // listItem.dataset.url = recipes[i].recipe.url);
-          // listItem.innerHTML = `${recipes[i].recipe.label}`
-          // recipeList.appendChild(listItem);
-          //  console.log(recipeList)
-        }
-      });
-  }
-}
-// changes
+//------------------------------------Calories info below -----------------
 function displayDetail(event) {
   if (event.target.matches("li")) {
-    // console.log(event.target.textContent)
-    // console.log(event.target.dataset.cal)
     let labelEl = document.createElement("h3")
     labelEl.innerHTML = `<a href=${event.target.dataset.url}>${event.target.textContent}</a>`
     infoCard.appendChild(labelEl)
 
     let calorieEl = document.createElement("p")
     calorieEl.textContent = ("Calories: " + event.target.dataset.cal)
-    infoCard.appendChild(calorieEl)
-    // let linkEl = document.createElement("a")
+    infoCard.appendChild(calorieEl)}}
 
-    // listItem.innerHTML = `<a href=${recipes[i].recipe.url}>${recipes[i].recipe.label}</a>`
-  }
-}
-//listItem.innerHTML = `<a href=${recipes[i].recipe.url}>${recipes[i].recipe.label}</a>`
-
-
-//seperate event listener for our ul
-// use event delegation to do event.target
-// create an element for that 3rd card
-// populate that element with the data from our data-attributes
-// and append it to that card
 proteinContainer.addEventListener('click', getApi)
 
 recipeList.addEventListener('click', displayDetail)
 
+//----------------------API below --------------------------------
 fetch(
   `https://edamam-recipe-search.p.rapidapi.com/search?q=${protein}`,
   nutritionAPI
@@ -91,21 +58,18 @@ fetch(
   .then((response) => response.json())
   .then((response) => console.log(response))
   .catch((err) => console.error(err));
-//
-
-// // Fitness Section
-// const fitnessOptions = {
-//   method: "GET",
-//   headers: {
-//     "X-RapidAPI-Key": "5ef0a389f2msh11866c287896cb0p101bc7jsn1c10fa4b94f1",
-//     "X-RapidAPI-Host": "calories-burned-by-api-ninjas.p.rapidapi.com",
-//   },
-// };
-
-// fetch(
-//   "https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned?activity=skiing",
-//   fitnessOptions
-// )
-//   .then((response) => response.json())
-//   .then((response) => console.log(response))
-//   .catch((err) => console.error(err));
+const fitnessOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "5ef0a389f2msh11866c287896cb0p101bc7jsn1c10fa4b94f1",
+    "X-RapidAPI-Host": "calories-burned-by-api-ninjas.p.rapidapi.com",
+  },
+};
+fetch(
+  "https://calories-burned-by-api-ninjas.p.rapidapi.com/v1/caloriesburned?activity=skiing",
+  fitnessOptions
+)
+  .then((response) => response.json())
+  .then((response) => console.log(response))
+  .catch((err) => console.error(err));
+//------------------------API above --------------------------------------
